1cff8e862f2b211609b4783138f510bc6563f0ed
Vegard Lillevoll <lilveg@gmail.com>
2016-06-09 19:00:19 +0000

    Added automatic refreshing of todo list when the file is changed by an external program

--- toadmin.txt.py
+++ toadmin.txt.py
@@ -23,7 +23,7 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE. 
 
-import pickle,os,re,datetime,argparse,string
+import pickle,os,re,datetime,argparse,string,signal,time
 
 # Parse command-line options
 parser = argparse.ArgumentParser(description="Automate certain todo list tasks")
@@ -31,6 +31,13 @@ parser.add_argument("--options_file", help="File to read and write options")
 parser.add_argument("--review", action="store_true", help="Perform review")
 parser.add_argument("--guided", action="store_true", help="Go through tasks needing attention step-by-step")
 args = parser.parse_args()
+
+# Used to see if todo.txt was modified from another program
+todo_last_modified = time.time()
+todo_externally_changed = False
+
+# Variable to store todos
+local_todos = None
 
 # Default data file location
 data_file = os.path.join(os.path.expanduser("~"), ".toadmin.txt")
@@ -219,9 +226,115 @@ def load_todos():
 
 
 def save_todos(local_todos):
+    global todo_last_modified
+
     with open(options["todo.txt-location"], "w") as local_todos_file:
         for todo in local_todos:
             local_todos_file.write(str(todo))
+
+    todo_last_modified = time.time()
+
+# Get string showing numbered, grouped tasks. Returns (str, index_list)
+def get_interactive_task_list(tasks):
+    next = []
+    today = []
+    scheduled = []
+    waiting = []
+    someday = []
+    new = []
+    other = []
+
+    return_string = ""
+
+    for task in local_todos:
+        if task.done:
+            continue
+
+        if not "state" in task.addons:
+            other.append(task)
+
+        else:
+            if task.addons['state'] == "next":
+                next.append(task)
+            
+            elif task.addons['state'] == "today":
+                today.append(task)
+            
+            elif task.addons['state'] == "scheduled":
+                scheduled.append(task)
+            
+            elif task.addons['state'] == "waiting":
+                waiting.append(task)
+            
+            elif task.addons['state'] == "someday":
+                someday.append(task)
+
+            elif task.addons['state'] == "new":
+                new.append(task)
+
+            else:
+                return_string += "Warning: unrecognized state: " + task.addons['state'] + "\n"
+                other.append(task)
+
+    scheduled.sort(key = lambda x: x.addons["due"])
+
+    index_list = next + today + scheduled + waiting + someday + new +other
+
+
+    states = {0: "Next", 
+            len(next): "Today", 
+            len(next) + len(today): "Scheduled",
+            len(next) + len(today) + len(scheduled): "Waiting",
+            len(next) + len(today) + len(scheduled) + len(waiting): "Someday",
+            len(next) + len(today) + len(scheduled) + len(waiting) + len(someday): "New",
+            len(next) + len(today) + len(scheduled) + len(waiting) + len(someday) + len(new): "Other"}
+
+    for i in range(len(index_list)):
+        if i in states:
+            return_string += "\n" + states[i] + "\n"
+
+        if index_list[i].addons["state"] == "scheduled":
+            return_string += (str(i).zfill(2) + ": (" + index_list[i].addons["due"].isoformat() + ") " + 
+                    index_list[i].human_str() + "\n")
+
+        else:
+            return_string += str(i).zfill(2) + ": " + index_list[i].human_str() + "\n"
+
+    return (return_string, index_list)
+
+# Used to check if todo.txt has changed while entering commands
+def interactive_check_todo_changes(signum, frame):
+    global local_todos, todo_last_modified, todo_externally_changed
+    if os.path.getmtime(options["todo.txt-location"]) > todo_last_modified:
+        todo_externally_changed = True
+        print("WARNING: todo.txt has been modified from another application. Reloading...")
+        local_todos = do_auto_actions(load_todos())
+        save_todos(local_todos)
+        (s, l) = get_interactive_task_list(local_todos)
+        print(s)
+        print("Todos reloaded.")
+        todo_last_modified = time.time()
+
+def do_auto_actions(todos):
+    for todo in todos:
+        # Tag new tasks as new
+        if not "state" in todo.addons and not todo.done:
+            todo.addons["state"] = "new"
+
+        # Complete tasks should be tagged as such
+        if todo.done:
+            todo.addons["state"] = "done"
+
+        # Set creation date for undated tasks
+        if not todo.created and not todo.done:
+            todo.created = datetime.date.today()
+
+        # Make scheduled tasks due today have state today
+        if ((todo.addons["state"] == "scheduled" or todo.addons["state"] == "waiting") and "due" in todo.addons and 
+                todo.addons["due"] == datetime.date.today()):
+            todo.addons["state"] = "today"
+
+    return todos
 
 # Load options
 if os.path.isfile(data_file):
@@ -241,25 +354,8 @@ if not "todo.txt-location" in options:
 # Load todos from todo.txt
 local_todos = load_todos()
 
-
-# Start action list
-for todo in local_todos:
-    # Tag new tasks as new
-    if not "state" in todo.addons and not todo.done:
-        todo.addons["state"] = "new"
-
-    # Complete tasks should be tagged as such
-    if todo.done:
-        todo.addons["state"] = "done"
-
-    # Set creation date for undated tasks
-    if not todo.created and not todo.done:
-        todo.created = datetime.date.today()
-
-    # Make scheduled tasks due today have state today
-    if ((todo.addons["state"] == "scheduled" or todo.addons["state"] == "waiting") and "due" in todo.addons and 
-            todo.addons["due"] == datetime.date.today()):
-        todo.addons["state"] = "today"
+# Perform automated actions
+local_todos = do_auto_actions(local_todos)
 
 next_todos = []
 today_todos = []
@@ -360,72 +456,16 @@ quit = False
 if args.review:
     # Enter interactive mode
     out = ""
+    signal.signal(signal.SIGALRM, interactive_check_todo_changes)
+    signal.setitimer(signal.ITIMER_REAL, 1, 1)
+
     while not quit:
         # Sort & save tasks
         local_todos.sort(key = lambda x: str(x))
         save_todos(local_todos)
 
-        next = []
-        today = []
-        scheduled = []
-        waiting = []
-        someday = []
-        new = []
-        other = []
-
-        for task in local_todos:
-            if task.done:
-                continue
-
-            if not "state" in task.addons:
-                other.append(task)
-
-            else:
-                if task.addons['state'] == "next":
-                    next.append(task)
-                
-                elif task.addons['state'] == "today":
-                    today.append(task)
-                
-                elif task.addons['state'] == "scheduled":
-                    scheduled.append(task)
-                
-                elif task.addons['state'] == "waiting":
-                    waiting.append(task)
-                
-                elif task.addons['state'] == "someday":
-                    someday.append(task)
-
-                elif task.addons['state'] == "new":
-                    new.append(task)
-
-                else:
-                    print("Warning: unrecognized state: " + task.addons['state'])
-                    other.append(task)
-
-        scheduled.sort(key = lambda x: x.addons["due"])
-
-        index_list = next + today + scheduled + waiting + someday + new +other
-
-
-        states = {0: "Next", 
-                len(next): "Today", 
-                len(next) + len(today): "Scheduled",
-                len(next) + len(today) + len(scheduled): "Waiting",
-                len(next) + len(today) + len(scheduled) + len(waiting): "Someday",
-                len(next) + len(today) + len(scheduled) + len(waiting) + len(someday): "New",
-                len(next) + len(today) + len(scheduled) + len(waiting) + len(someday) + len(new): "Other"}
-
-        for i in range(len(index_list)):
-            if i in states:
-                print("\n" + states[i])
-
-            if index_list[i].addons["state"] == "scheduled":
-                print(str(i).zfill(2) + ": (" + index_list[i].addons["due"].isoformat() + ") " + 
-                        index_list[i].human_str())
-
-            else:
-                print(str(i).zfill(2) + ": " + index_list[i].human_str())
+        (s, index_list) = get_interactive_task_list(local_todos)
+        print(s)
 
         if out != "":
             print(out)
@@ -437,6 +477,11 @@ if args.review:
         except KeyboardInterrupt:
             quit = True
             continue
+
+        # If todo.txt is modified by another program, make sure we have correct task list numbers
+        if todo_externally_changed:
+            (s, index_list) = get_interactive_task_list(local_todos)
+            todo_externally_changed = False
 
         cmd = inp.split(" ")
 
